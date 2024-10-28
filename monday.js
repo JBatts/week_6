3be2c022b4690b04d1d949248882ed1f7cff8160
Jalen Battle <jalenbattle78@gmail.com>
2024-10-28 10:11:30 -0400

    first commit

---
+++ monday.js
@@ -0,0 +1,14 @@
+
+const fruit = ["apple", "banana", "cherrry"];
+
+fruit.forEach((f, i, a) => console.log(f));
+
+
+// No Variable
+[111, 222, 333].forEach(c => console.log(c));
+
+["apple", "banana", "cherry"].forEach((f,i,a) => console.log(i+1, f, a));
+
+
+// Extra Credit - show diff between current value and previous value
+[11, 14, 16, 25].forEach((n, i, a) => i ? console.log(n - a[i - 1]) : 0); 
